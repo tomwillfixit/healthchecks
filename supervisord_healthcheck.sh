9ac6ca36d6b116e72c4f51354a27785707e2e870
Thomas Shaw <tomwillfixit@users.noreply.github.com>
2019-09-15 00:33:29 +0100

    initial commit

---
+++ supervisord_healthcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Simple healthcheck which compares number of services registered in supervisord and the number of running services.
+
+number_of_services=$(supervisorctl status |wc -l)
+running_services=$(supervisorctl status |grep RUNNING |wc -l)
+
+if [ ${number_of_services} -eq 0 ];then
+    exit 1
+fi
+
+if [ ${number_of_services} -eq ${running_services} ];then
+    exit 0
+else
+    exit 1
+fi
