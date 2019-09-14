21be56d0dba862f27637df2328fe3374b8b1b3e0
Thomas Shaw <tomwillfixit@users.noreply.github.com>
2019-09-15 00:57:06 +0100

    add some logging to docker logs

--- supervisord_healthcheck.sh
+++ supervisord_healthcheck.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Simple healthcheck which compares number of services registered in supervisord and the number of running services.
-
-echo "$(date) : $(supervisorctl status)" >> /tmp/health.log
+# Failing healthchecks info written to docker logs output.
 
 number_of_services=$(supervisorctl status |wc -l)
 running_services=$(supervisorctl status |grep RUNNING |wc -l)
 
 if [ ${number_of_services} -eq 0 ];then
+    echo "Number of Services Running is 0" >> /proc/1/fd/0
     exit 1
 fi
 
 if [ ${number_of_services} -eq ${running_services} ];then
     exit 0
 else
+    echo "Number of services registered : ${number_of_services}" >> /proc/1/fd/0
+    echo "Number of running services    : ${running_services}" >> /proc/1/fd/0
     exit 1
 fi
